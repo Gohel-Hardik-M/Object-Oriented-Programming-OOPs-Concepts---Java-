8cf5fc15aca087e9a58ff3468a98153522719e94
GOHEL HARDIK <hardikmg21@gmail.com>
2025-08-11 09:02:55 +0530

    Update Car.java

--- 01-Classes & Objects /Car.java
+++ 01-Classes & Objects /Car.java
@@ -1,1 +1,20 @@
+class car{
 
+  String make;
+  String model;
+  int year;
+  void startEngine(){
+    System.out.println(make + " " + model + "Engine started......");
+  }
+}
+
+class Main{
+  public static void main(String[] args){
+    car  car1 = new car();
+    car1.make = "TOYOTA";
+    car1.model = "SUV";
+    car1.year = "1990";
+
+    car1.startEngine();
+  }
+}
